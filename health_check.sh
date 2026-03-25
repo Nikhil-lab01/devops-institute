39855034b0a949fddb211c5dc388c63cdf7c3d8c
Nikhil <nikhil@chipincorp.com>
2026-03-25 12:03:36 +0000

    My first copy

---
+++ health_check.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "---SERVER HEALTH REPORT ---"
+date
+echo ""
+echo "1. DISK SPACE USAGE."
+df -h | grep "/$"
+echo ""
+echo "2 ACTIVE NETWROK PORTS"
+netstat -tuln | head -n 5
+echo""
+echo "3 TOP MEAMORY PROCESS"
+ps aux --sort=-%mem | head -n 3
+echo ""
+echo "Report Complete"
