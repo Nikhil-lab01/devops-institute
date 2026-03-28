0c9655c67b1385993679bfebf9e27765603ce79f
Nikhil <nikhil@chipincorp.com>
2026-03-28 11:05:29 +0000

    Add network connectivity and port diagnostics

--- health_check.sh
+++ health_check.sh
@@ -12,5 +12,19 @@ echo "3 TOP MEAMORY PROCESS"
 ps aux --sort=-%mem | head -n 3
 echo ""
 echo "Checking DNS..." && cat /etc/resolv.conf
+echo ""
+echo "--- Network Security Diagnostics ---"
+#1. Check internet Connectivity (Ping Google DNS)
+if ping -c 1 8.8.8.8 &> /dev/null;then
+   echo "[OK] Internet Connectivity: Online"
+else
+   echo "[ERROR] Internet Connectivity: Offline"
+fi
+
+#2. Check for open ports (Internal Security Scan)
+# This mimics a basic 'netstat'to see what services are listening
+echo "Active Listening Ports"
+echo ""
 echo "Report Complete"
+echo""
 echo "Bye"
